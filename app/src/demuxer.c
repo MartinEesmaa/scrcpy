12ce2ad9dface1e2ee31d5d3809456042b8fd866
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-12-30 13:48:59 +0200

    i forgot that fix it

--- app/src/demuxer.c
+++ app/src/demuxer.c
@@ -229,8 +229,7 @@ run_demuxer(void *data) {
     // Config packets must be merged with the next non-config packet only for
     // H.26x
     bool must_merge_config_packet = raw_codec_id == SC_CODEC_ID_H264
-                                 || raw_codec_id == SC_CODEC_ID_H265
-                                 || raw_codec_id == SC_CODEC_ID_APV;
+                                 || raw_codec_id == SC_CODEC_ID_H265;
 
     struct sc_packet_merger merger;
 
